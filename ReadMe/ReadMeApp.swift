3707afce0a69b05ca85c02a1d16d6affeafcf442
Adya Naufal Fikri <adyanaufalf@gmail.com>
2022-09-17 17:26:03 +0700

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Adya Naufal Fikri on 17/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
